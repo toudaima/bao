52e3995252109351535a60a3f4fa0b8313299aae
liling <876959376@qq.com>
2020-03-20 17:29:54 +0800

    2020-03-20 CompletableFuture测试 修改xmind

---
+++ src/main/java/com/ligouhai/bao/test/socket/NormalSocket.java
@@ -0,0 +1,14 @@
+package com.ligouhai.bao.test.socket;
+
+import java.util.HashMap;
+
+/**
+ * @author ligouhai
+ * @date 2020-03-20 10:20
+ * @description 传统socket
+ */
+public class NormalSocket {
+    public static void main(String[] args) {
+
+    }
+}
